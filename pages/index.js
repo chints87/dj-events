4d80ad104c1c65d5c7123a5570a8b2bcfd962fd2
Chintan <cgandhi4@gmail.com>
2022-02-08 13:13:41 +0530

    Paging and Routing

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,5 @@
-import Layout from '@/components/Layout';
-
-export default function EventsPage() {
+export default function HomePage() {
   return (
-    <Layout title="Events Page" description="events" keywords="is,a,event">
-      <h1>Events</h1>
-    </Layout>
+    <h1>HomePage</h1>
   );
 }
-
-/* This function is executed first before the
- component function */
-export async function getStaticProps() {
-  /* Pre-generate a page i.e the
-  HTML and data - prepared from server side -
-  content during build time */
-
-  /* Run server side code */
-
-  /* Code doesnt get bundled, which is
-     sent to the client side */
-
-  /* Prefetch data before this component
-     is created */
-  return {
-    props: {},
-  };
-}
-
-/* THIS CODE IS EXECUTED ONLY DURING
-BUILD TIME OR DEV SERVER */
