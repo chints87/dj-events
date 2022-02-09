c1a3099304182d9bbc530e35bc46343f25f6f853
Chintan <cgandhi4@gmail.com>
2022-02-09 09:11:57 +0530

    Add events to EventsPage

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,30 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
-    <Layout title="Events">
-      <h1>EventsPage</h1>
+    <Layout title="All Events">
+      <h1>All Events</h1>
+      { events.length === 0 && <p>No Events</p> }
+
+      {events.map((evt) => <EventItem key={evt.id} evt={evt} />)}
     </Layout>
   );
+}
+
+// getServerSideProps sends a request to fetch data whenever
+// navigated or reloaded to the page containing it
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    // Over here since getStateProps the props are received
+    // only at built time, any updates made to the event will
+    // need to be seen and for that revalidate is used.
+    revalidate: 1,
+  };
 }
