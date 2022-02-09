c0d3e77d5288f2977bb100cdb3fbf1fcc24acfad
Chintan <cgandhi4@gmail.com>
2022-02-09 10:24:48 +0530

    Update Event Page with api calls

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -10,7 +10,20 @@ export default function Eventpage({ evt }) {
   );
 }
 
-export async function getServerSideProps({ query: { slug } }) {
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/api/events/${slug}`);
   const events = await res.json();
 
@@ -18,5 +31,16 @@ export async function getServerSideProps({ query: { slug } }) {
     props: {
       evt: events[0],
     },
+    revalidate: 1,
   };
 }
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   };
+// }
