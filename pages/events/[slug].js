b62bfe433470c12ae9627c513b2edcfafcaada5e
Chintan <cgandhi4@gmail.com>
2022-02-09 10:03:16 +0530

    Add slug

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function Eventpage() {
+export default function Eventpage({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.slug}</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
 }
