e99d7cec8bebc933b5f360e326ae6b37c5d53bc3
Chintan <cgandhi4@gmail.com>
2022-02-09 10:05:52 +0530

    Updated index page for landing and events page

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,5 +1,10 @@
 const { events } = require('./data.json');
 
 export default function handler(req, res) {
-  res.status(200).json(events);
+  if (req.method === 'GET') {
+    res.status(200).json(events);
+  } else {
+    res.setHeader(['Allow', ['GET']]);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Layout from '@/components/Layout';
 import { API_URL } from '@/config/index';
 import EventItem from '@/components/EventItem';
@@ -9,6 +10,12 @@ export default function HomePage({ events }) {
       { events.length === 0 && <p>No Events</p> }
 
       {events.map((evt) => <EventItem key={evt.id} evt={evt} />)}
+
+      { events.length > 0 && (
+      <Link href="/events">
+        <a style={{ textDecoration: 'underline' }}>View all events(takes them to events page)</a>
+      </Link>
+      )}
     </Layout>
   );
 }
@@ -21,7 +28,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     // Over here since getStateProps the props are received
     // only at built time, any updates made to the event will
     // need to be seen and for that revalidate is used.
