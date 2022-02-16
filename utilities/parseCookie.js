f2346b0a7fa4fa82382e42c1c6b9928f754a1933
Chintan <cgandhi4@gmail.com>
2022-02-16 15:49:53 +0530

    Add parseCookie function

--- utilities/parseCookie.js
+++ utilities/parseCookie.js
@@ -1,3 +1,7 @@
 import cookie from 'cookie';
 
-export const parseCookie = (req) => cookie.parse(req ? req.headers.cookie || '' : '');
+// export const parseCookie = (req) => cookie.parse(req ? req.headers.cookie || '' : '');
+
+export function parseCookie(req) {
+  return cookie.parse(req ? req.headers.cookie || '' : '');
+}
