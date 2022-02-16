d0b86c4d1a0d1b7f43dacb242ee3294eab1900e5
Chintan <cgandhi4@gmail.com>
2022-02-16 15:57:17 +0530

    Modify to getServersSideProps

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -31,20 +31,6 @@ export default function Eventpage({ evt }) {
   return (
     <Layout>
       <h1>{evt.slug}</h1>
-      <Link href={`/events/edit/${evt.id}`}>
-        <a>
-          <FaPencilAlt />
-          {' '}
-          Edit
-        </a>
-      </Link>
-      <Link href="#">
-        <a onClick={deleteEvent} role="presentation">
-          <FaTimes />
-          {' '}
-          Delete
-        </a>
-      </Link>
       <p>Other event info</p>
       <Link href="/events">
         <a>Go back</a>
@@ -53,41 +39,42 @@ export default function Eventpage({ evt }) {
   );
 }
 
-// Calling events from API then extracting the
-// slug parameters for all events into a variable paths
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/events`);
-  const events = await res.json();
+// // Calling events from API then extracting the
+// // slug parameters for all events into a variable paths
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`);
+//   const events = await res.json();
 
-  const paths = events.map((evt) => ({
-    params: { slug: evt.slug },
-  }));
-  return {
-    paths,
-    fallback: true,
-  };
-}
+//   const paths = events.map((evt) => ({
+//     params: { slug: evt.slug },
+//   }));
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// }
 
-// This function takes path as a parameter
-// Does this require extra memory?
-export async function getStaticProps({ params: { slug } }) {
-  const res = await fetch(`${API_URL}/events?slug=${slug}`);
-  const events = await res.json();
-
-  return {
-    props: {
-      evt: events[0],
-    },
-    revalidate: 1,
-  };
-}
-// export async function getServerSideProps({ query: { slug } }) {
-//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+// // This function takes path as a parameter
+// // Does this require extra memory?
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`);
 //   const events = await res.json();
 
 //   return {
 //     props: {
 //       evt: events[0],
 //     },
+//     revalidate: 1,
 //   };
 // }
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
+  console.log(events);
+
+  return {
+    props: {
+      evt: events[0],
+    },
+  };
+}
